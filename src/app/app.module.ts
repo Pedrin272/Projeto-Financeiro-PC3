18d0ad54f751195a3091f726b207364205788af8
Pedro Henrique T.O <pepe.goval@gmail.com>
2022-09-04 18:20:35 -0300

    modified this package

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,9 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
+import { MaterialExampleModule } from 'src/material.module';
 import { AppComponent } from './app.component';
 import { PaginaPrincipalComponent } from './components/pagina-principal/pagina-principal.component';
 import { Pagina1Component } from './components/pagina1/pagina1.component';
 import { Pagina2Component } from './components/pagina2/pagina2.component';
-import { MatTableModule } from '@angular/material/table';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { FormsModule } from '@angular/forms';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatInputModule } from '@angular/material/input';
-import {MatCardModule} from '@angular/material/card';
-
-import { MaterialExampleModule } from 'src/material.module';
-import { HttpClientModule } from '@angular/common/http';
 import { ProdutoFormComponent } from './components/produto-form/produto-form.component';
 
 @NgModule({
@@ -25,16 +15,7 @@ import { ProdutoFormComponent } from './components/produto-form/produto-form.com
     ProdutoFormComponent,
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    FormsModule,
-    MatTableModule,
-    MatFormFieldModule,
-    MatInputModule,
-    HttpClientModule,
-    MatCardModule,
-    MaterialExampleModule,
+    MaterialExampleModule
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/components/page-dialog/page-dialog.component.ts
+++ src/app/components/page-dialog/page-dialog.component.ts
@@ -1,28 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder,FormControl } from '@angular/forms';
-import { Products } from 'src/app/models/products/products';
-import { ProductsService } from 'src/app/models/products/products.service';
-import {MatCardModule} from '@angular/material/card';
+import { FormBuilder, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-page-dialog',
   templateUrl: './page-dialog.component.html',
-  styleUrls: ['./page-dialog.component.scss']
+  styleUrls: ['./page-dialog.component.scss'],
 })
 export class PageDialogComponent implements OnInit {
   Form = this.formBuilder.group({
-    id:new FormControl<string>("", { nonNullable: true }),
-    nome: new FormControl<string>("", { nonNullable: true }),
+    id: new FormControl<string>('', { nonNullable: true }),
+    nome: new FormControl<string>('', { nonNullable: true }),
     valorVenda: new FormControl<number>(0, { nonNullable: true }),
     estoque: new FormControl<number>(0, { nonNullable: true }),
     // ('', { nonNullable: true }) só isso o Ts já entende
   });
 
-  constructor(private formBuilder: FormBuilder,
-    ) { }
+  constructor(private formBuilder: FormBuilder) {}
 
-  ngOnInit() {
-  }
-
-
+  ngOnInit() {}
 }
