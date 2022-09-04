b077b11ac133e1e45ad1590f20846b4867bb3275
Pedro Henrique T.O <pepe.goval@gmail.com>
2022-09-04 19:57:08 -0300

    umas merda

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,21 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { BrowserModule } from '@angular/platform-browser';
 import { MaterialExampleModule } from 'src/material.module';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PaginaPrincipalComponent } from './components/pagina-principal/pagina-principal.component';
 import { Pagina1Component } from './components/pagina1/pagina1.component';
 import { Pagina2Component } from './components/pagina2/pagina2.component';
 import { ProdutoFormComponent } from './components/produto-form/produto-form.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @NgModule({
   declarations: [
@@ -15,9 +26,20 @@ import { ProdutoFormComponent } from './components/produto-form/produto-form.com
     ProdutoFormComponent,
   ],
   imports: [
-    MaterialExampleModule
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatDialogModule,
+    MatButtonModule,
+    HttpClientModule,
+    MaterialExampleModule,
+    MatCardModule,
   ],
-  providers: [],
+  providers: [MaterialExampleModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
